830f9cfe3af73c4130748bc6e09165728c34b878
Luna L <hanken95@gmail.com>
2024-09-10 15:21:44 +0200

    Finished projekt

---
+++ StaffRegister/Employee.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace StaffRegister
+{
+    public class Employee
+    {
+		private string firstName;
+		private string lastName;
+        private int salary;
+
+        public Employee(string _firstName, string _lastName, int _salary)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            salary = _salary;
+        }
+
+        public string Name
+		{
+			get { return firstName + " " + lastName; }
+		}
+
+
+		public int Salary
+		{
+			get { return salary; }
+			set { salary = value; }
+		}
+
+	}
+}
--- StaffRegister/Program.cs
+++ StaffRegister/Program.cs
@@ -1,10 +1,96 @@
-﻿namespace StaffRegister
+﻿
+
+
+namespace StaffRegister
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Employee> employees = new List<Employee>();
+
+            bool exitLoop = false;
+            while (!exitLoop)
+            {
+                Console.WriteLine("1: Enter new employee details");
+                Console.WriteLine("2: View current employees");
+                Console.WriteLine("3: Exit program");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        employees.Add(EmployeeCreator());
+                        break;
+                    case '2':
+                        ViewEmployees(employees);
+                        break;
+                    case '3':
+                        exitLoop = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input. Press any key to try again");
+                        Console.ReadKey(true);
+                        break;
+                }
+                Console.Clear();
+            }
         }
+
+
+        private static Employee EmployeeCreator()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter first name");
+            var firstName = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Enter surname");
+            var surName = Console.ReadLine();
+
+            int salary = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter salary");
+                if (int.TryParse(Console.ReadLine(), out salary))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Wrong input. Press a key to restart salary input");
+                    Console.ReadKey();
+                }
+            }
+
+            return new Employee(firstName, surName, salary);
+        }
+
+        private static void ViewEmployees(List<Employee> employees)
+        {
+            Console.Clear();
+            if (employees.Count > 0)
+            {
+                Console.WriteLine("Employees:");
+                Console.WriteLine();
+
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine("Name:    " + employee.Name);
+                    Console.WriteLine("Salary:  " + employee.Salary);
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no employees registered");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any button to go back to the previous menu");
+            Console.ReadKey(true);
+        }
+
     }
+
 }
