af3e3ad17754afbd06c66f92ccfd991ffe00e7fc
Luna L <hanken95@gmail.com>
2024-09-10 14:07:58 +0200

    Add project files.

---
+++ StaffRegister/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StaffRegister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
